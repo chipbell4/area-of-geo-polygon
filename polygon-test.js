d2bc5b2f5fd721cf149f3d2494decab3827b5a5a
Chip Bell <chip.bell@spinen.com>
2015-11-22 17:39:41 -0500

    Stubbing out the polygon test file

---
+++ polygon-test.js
@@ -0,0 +1,17 @@
+var calculateError = require('./calculate-error');
+var project = require('./projection');
+var area = require('area-polygon');
+
+/**
+ * Calculates the error from our projection for the specified polygon
+ */
+module.exports = function(polygon, expectedArea) {
+  // first, project
+  var projectedPolygon = polygon.map(project);
+
+  // now the area
+  var polygonArea = area(projectedPolygon);
+
+  // spit back the associated error
+  return calculateError(expectedArea, polygonArea);
+};
