2b05277e0a4f29c42a3b647e509683da5fe45fee
Chip Bell <chip.bell@spinen.com>
2015-11-22 18:09:36 -0500

    Trying out a projection

--- projection.js
+++ projection.js
@@ -2,5 +2,13 @@
  * Projects a point to a different coordinate system
  */
 module.exports = function(point) {
-  return point; // a no-op for now
+  var latitude = point.x, longitude = point.y;
+  if(point instanceof Array) {
+    latitude = point[0], longitude = point[1];
+  }
+
+  return {
+    x: longitude / 360 * 24901,
+    y: Math.sin(latitude * Math.PI / 180) * 4000
+  };
 };
