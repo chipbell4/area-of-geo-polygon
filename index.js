bee9820a0fd6972183b7c3c61361428d83b2d567
Chip Bell <chip.bell@spinen.com>
2015-11-22 17:49:03 -0500

    Adding a test case

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var polygonTest = require('./polygon-test');
 
 var testCases = [
+  {
+    name: 'Colorado',
+    expectedArea: 104185,
+    polygon: [
+      [41.00477542222949, -109.05029296875],
+      [40.98819156349393, -102.030029296875],
+      [37.00255267215955, -102.06298828125],
+      [36.99377838872517, -109.05029296875]
+    ],
+  }
 ];
 
 console.log('RUNNING TESTS');
